d31c4fe9df3da902382f76b19012dc91056de4d5
Vipul Singh <neoeahit@users.noreply.github.com>
2017-02-05 23:49:52 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
 ##Merges the training and the test sets to create one data set
 
+# Running the script at the same location as "UCI HAR Dataset"
+
 x_train<- read.table("UCI HAR Dataset/train/X_train.txt")
 x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
@@ -19,10 +21,11 @@ names(x) <- features_name[,2]
 data<- cbind(subjects, y,x)
 
 ## Extracts only the measurements on the mean and standard deviation for each measurement.
-
+ # Learnt \ does not work, note to self use \\ over \
 sampled_index <- grep("-mean\\(\\)|-std\\(\\)", features_name[,2], ignore.case=TRUE)
 x<-x[,sampled_index]
-## Uses descriptive activity names to name the activities in the data set( names are already descriptive)
+## Uses descriptive activity names to name the activities in the data set
+## (names are already descriptive, not sure what more to change in names)
 activities[, 2] = as.character(activities[, 2])
 y[,1] <- activities[y[,1],2]
 
@@ -36,5 +39,8 @@ sampled_x <- clean_data_with_descriptive_name[, 3:dim(clean_data_with_descriptiv
 aggregated_data <- aggregate(sampled_x, by=list(clean_data_with_descriptive_name$Subject, clean_data_with_descriptive_name$Activity), FUN=mean)
 names(aggregated_data)[1] <- "Subject"
 names(aggregated_data)[2] <- "Activity"
+
+#write.table is not included in the script but incase it was and i missed it, it was supposed to be used as
+#write.table(aggregated_data, "name_of_the_file.txt", row.name=FALSE)
 
 
